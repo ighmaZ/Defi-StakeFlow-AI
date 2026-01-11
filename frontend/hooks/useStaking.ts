27fd3770d013c65916adfa7b099c0e9eab3d8669
ighmaz <ighmazcool@gmail.com>
2026-01-11 11:30:38 +0530

    feat:useStaking

---
+++ frontend/hooks/useStaking.ts
@@ -0,0 +1,42 @@
+import {
+  STAKE_FLOW_VAULT_ADDRESS,
+  STAKEFLOW_VAULT_ABI,
+} from "@/config/contracts";
+import { useConnection, useReadContract } from "wagmi";
+import { StakerInfo } from "@/types";
+
+export function useStaking() {
+  const { address, isConnected } = useConnection();
+
+  //fetch staked amount
+
+  const {
+    data: stakerInfo,
+    isLoading: isLoadingStaking,
+    error: stakingError,
+    refetch: refetchStaking,
+  } = useReadContract({
+    address: STAKE_FLOW_VAULT_ADDRESS,
+    abi: STAKEFLOW_VAULT_ABI,
+    functionName: "getStakerInfo",
+    args: address ? [address] : undefined,
+    query: {
+      enabled: !!address && isConnected,
+      refetchInterval: 10000,
+      staleTime: 10000,
+    },
+  });
+
+  const stakedInfo = stakerInfo as StakerInfo | undefined;
+  const stakedAmount = stakedInfo?.stakedAmount || 0n;
+  const stakedTimestamp = stakedInfo?.stakedTimestamp || 0;
+  const pendingRewards = stakedInfo?.pendingRewardAmount || 0n;
+  return {
+    stakedAmount,
+    stakedTimestamp,
+    pendingRewards,
+    isLoading: isLoadingStaking,
+    refetchStaking,
+    error: stakingError,
+  };
+}
